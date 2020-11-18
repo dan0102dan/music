0112124c54d39da339c79113219ed276dbc08565
Daniil Davydov <dan0102dan@MacBook-Pro.local>
2020-11-18 14:52:18 +0300

    Updates

---
+++ precache-manifest.3c9800238493bfb8aee38be3e978e06d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335adaa6e60403cd165d4b1b3b5e7511",
+    "url": "/music/index.html"
+  },
+  {
+    "revision": "07b5f3d98508455d8272",
+    "url": "/music/static/css/2.62385eee.chunk.css"
+  },
+  {
+    "revision": "2f7372f419f68843a039",
+    "url": "/music/static/css/main.3c30cfb9.chunk.css"
+  },
+  {
+    "revision": "07b5f3d98508455d8272",
+    "url": "/music/static/js/2.2ef97e60.chunk.js"
+  },
+  {
+    "revision": "308550227c98d9d10aa54bd6a60fea4c",
+    "url": "/music/static/js/2.2ef97e60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f7372f419f68843a039",
+    "url": "/music/static/js/main.57220795.chunk.js"
+  },
+  {
+    "revision": "3ac88e11f787f57f2f24",
+    "url": "/music/static/js/runtime-main.01faecc1.js"
+  },
+  {
+    "revision": "cd5eac4280ad9ef77bb0a0fafd68f3fd",
+    "url": "/music/static/media/AppleMusic.cd5eac42.svg"
+  },
+  {
+    "revision": "e359ad39480a3ed8d413d0a663bce8a7",
+    "url": "/music/static/media/Spotify.e359ad39.svg"
+  },
+  {
+    "revision": "deac2f88018a907c02c1cdd6a640542e",
+    "url": "/music/static/media/Yandex.deac2f88.svg"
+  },
+  {
+    "revision": "d549df088e53fea4eb24a8f66dbdc30b",
+    "url": "/music/static/media/ava.d549df08.png"
+  },
+  {
+    "revision": "0b93970850799fc8c3d29c5a73b18d7e",
+    "url": "/music/static/media/empty.0b939708.svg"
+  },
+  {
+    "revision": "6dd30fb5ff10b0c2164e59bc687ea4e8",
+    "url": "/music/static/media/error.6dd30fb5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/release/precache-manifest.3511258bf6162f0e0f489c4d8ae41ea7.js"
+  "/music/precache-manifest.3c9800238493bfb8aee38be3e978e06d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/release/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/music/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
